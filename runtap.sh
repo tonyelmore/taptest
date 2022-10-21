9ad42da3916779f5a8b730c1b8dee6de498fed8f
Tony Elmore <tony@tonyelmore.com>
2022-10-21 09:23:30 -0500

    Change message with failed tests

--- runtap.sh
+++ runtap.sh
@@ -2,6 +2,6 @@ tanzu apps workload create taptest \
 --git-repo https://github.com/tonyelmore/taptest \
 --git-branch main \
 --type web \
---label app.kubernetes.io/part-of=tanzu-java-web-app \
+--label app.kubernetes.io/part-of=tap-test-app \
 --yes \
 --namespace telmore
