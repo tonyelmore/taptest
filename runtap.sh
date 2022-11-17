01db812b9c601b20830ff5db08d3b96433eba859
Tony Elmore <tony@tonyelmore.com>
2022-11-16 18:01:19 -0600

    trigger pipeline

--- runtap.sh
+++ runtap.sh
@@ -4,6 +4,7 @@ tanzu apps workload create taptest \
 --type web \
 --label app.kubernetes.io/part-of=tap-test-app \
 --label apps.tanzu.vmware.com/has-tests=true \
+--annotation "autoscaling.knative.dev/minScale=1" \
 --service-account default \
 --yes \
 --namespace telmore
